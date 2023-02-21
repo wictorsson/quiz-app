4d1f10e927476d8cf90165336cc75bce0e8dd49e
Fredrik Wictorsson <wictorsson@gmail.com>
2023-02-21 18:01:53 +0100

    add BEM

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const mainElement = document.querySelector('[data-js="main"]');
-console.log(mainElement);
-mainElement.classList.add("dark");
 
-const answerButtonElement = document.querySelector("#answer-button");
-const answerTextElement = document.querySelector("#answer-text");
+const questionCards = document.querySelectorAll(".question-card");
+const answerButtonElement = document.querySelectorAll(
+  ".question-card--answer-button"
+);
+const answerTextElement = document.querySelectorAll(
+  ".question-card--answer-text"
+);
 
-answerButtonElement.addEventListener("click", function (event) {
-  if (answerButtonElement.textContent === "Show Answer") {
-    answerButtonElement.textContent = "Hide Answer";
-    answerTextElement.textContent = "Flex-direction";
-  } else if ((answerButtonElement.textContent = "Hide Answer")) {
-    answerButtonElement.textContent = "Show Answer";
-    answerTextElement.textContent = "";
-  }
+// for (var i = 0; i < answerButtonElement.length; i++) {
+answerButtonElement.forEach((button) => {
+  button.addEventListener("click", () => {
+    const answerText = button.nextElementSibling;
+    answerText.classList.toggle("hidden");
+
+    if (button.textContent === "Show Answer") {
+      button.textContent = "Hide Answer";
+      answerTextElement.textContent = "Flex-direction";
+    } else if ((button.textContent = "Hide Answer")) {
+      button.textContent = "Show Answer";
+      answerTextElement.textContent = "";
+    }
+  });
 });
