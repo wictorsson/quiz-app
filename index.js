fc3d20f75a9353dd51f26a4614e74d2b1da1b959
Fredrik Wictorsson <wictorsson@gmail.com>
2023-02-22 13:55:00 +0100

    add for loop

--- index.js
+++ index.js
@@ -8,11 +8,25 @@ const answerTextElement = document.querySelectorAll(
   ".question-card--answer-text"
 );
 
-// for (var i = 0; i < answerButtonElement.length; i++) {
-answerButtonElement.forEach((button) => {
+for (let i = 0; i < answerButtonElement.length; i++) {
+  answerButtonElement[i].addEventListener("click", () => {
+    answerTextElement[i].classList.toggle("hidden");
+
+    if (button.textContent === "Show Answer") {
+      button.textContent = "Hide Answer";
+      answerTextElement.textContent = "Flex-direction";
+    } else if ((button.textContent = "Hide Answer")) {
+      button.textContent = "Show Answer";
+      answerTextElement.textContent = "";
+    }
+  });
+}
+
+/*
+answerButtonElement.forEach((button, i) => {
   button.addEventListener("click", () => {
-    const answerText = button.nextElementSibling;
-    answerText.classList.toggle("hidden");
+    // const answerText = button.nextElementSibling;
+    answerTextElement[i].classList.toggle("hidden");
 
     if (button.textContent === "Show Answer") {
       button.textContent = "Hide Answer";
@@ -23,3 +37,4 @@ answerButtonElement.forEach((button) => {
     }
   });
 });
+*/
