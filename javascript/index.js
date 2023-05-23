94f91571313e05fff612296eb10dce67383aa4f8
Fredrik Wictorsson <wictorsson@gmail.com>
2023-05-23 17:43:29 +0200

    refactor code

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,89 @@
-// TODO
-// split js into separate files,
-// reset button in settings - Reset counter etc
+import { questionListFile } from "./questions.js";
 
 const bodyElement = document.querySelector('[data-js="body"]');
 const bookmarkBodyPage = document.querySelector(".bookmark-body");
 const darkModeToggle = document.querySelector("#darkmode-switch");
 
-//SET bookmark variables
-const bookmarkElement = document.querySelectorAll(".question-card--bookmark");
-const bookmarkCounter = document.querySelector(".counter-box--bookmark-number");
-
 // SET form variables
 const form = document.querySelector('[data-js="form"]');
-
 const questionElement = document.querySelector('[data-js="questionMessage"]');
 const amountLeftQuestion = document.querySelector(
   '[data-js="amountLeftQuestion"]'
 );
 const answerElement = document.querySelector('[data-js="answerMessage"]');
 const amountLeftAnswer = document.querySelector('[data-js="amountLeftAnswer"]');
+const main = document.querySelector('[data-js="main"]');
 
-//CALL init functions
-ResetAnswerButton();
-ResetBookmark();
+var questionList;
 
+if (bookmarkBodyPage) {
+  questionList = questionListFile.filter(function (obj, index) {
+    const bookmarkIndex = localStorage.getItem("bookMarkindex" + index);
+
+    return bookmarkIndex === "true";
+  });
+  if (questionList.length < 1) {
+    const main = document.querySelector('[data-js="main"]');
+    const cardSection = document.createElement("section");
+    main.append(cardSection);
+    cardSection.className = "question-card";
+    const questionParagraph = document.createElement("p");
+    cardSection.append("You don't have any bookmarks");
+  }
+} else {
+  questionList = [...questionListFile];
+}
+
+// Create question cards
+if (main) {
+  questionList.forEach((card, index) => {
+    const main = document.querySelector('[data-js="main"]');
+    const cardSection = document.createElement("section");
+    main.append(cardSection);
+    cardSection.className = "question-card";
+    const questionParagraph = document.createElement("p");
+    cardSection.append(questionParagraph);
+
+    if (!bookmarkBodyPage) {
+      const bookmarkArea = document.createElement("a");
+      cardSection.append(bookmarkArea);
+
+      const bookmarkImage = document.createElement("img");
+      bookmarkArea.append(bookmarkImage);
+      bookmarkImage.className = "question-card--bookmark";
+      bookmarkImage.src = "./assets/bookmark.png";
+    }
+    //GET question text by classname
+    questionParagraph.textContent = questionList[index].question;
+
+    const answerButton = document.createElement("button");
+    cardSection.append(answerButton);
+    answerButton.className = "question-card--answer-button";
+    answerButton.textContent = "Your Answer";
+
+    const answerParagraph = document.createElement("p");
+    cardSection.append(answerParagraph);
+    //GET answer text by classname
+    answerParagraph.textContent = questionList[index].answer;
+    answerParagraph.className = "question-card--answer-text answer--hidden";
+
+    if (card.hasOwnProperty("tags")) {
+      // Accessing the 'tags' property
+      const tags = card.tags;
+      const tagList = document.createElement("ul");
+      cardSection.append(tagList);
+      tagList.className = "question-card--categories-list";
+      tags.forEach((tag) => {
+        const tagElement = document.createElement("li");
+        tagList.append(tagElement);
+        tagElement.className = "question-card--categories-list--item";
+        tagElement.textContent = tag;
+      });
+    }
+  });
+}
+
+//Char counter
 if (questionElement || answerElement) {
   const maxLength = questionElement.getAttribute("maxlength");
   amountLeftQuestion.innerText = maxLength - questionElement.value.length;
@@ -44,31 +104,30 @@ if (localStorage.getItem("darkMode") == "true") {
   bodyElement.classList.remove("dark");
 }
 
+// Attach add listeners
+ResetAnswerButton();
+ResetBookmark();
+
 //SET bookmark icon filled/unfilled on load
-if (bookmarkElement) {
-  for (let i = 0; i < bookmarkElement.length; i++) {
-    if (localStorage.getItem("bookMarkindex" + i) === "true") {
-      bookmarkElement[i].setAttribute("src", "./assets/bookmark_filled.png");
-      if (bookmarkBodyPage) {
-        bookmarkElement[i].parentNode.classList.add("hidden");
-      }
-    } else {
-      bookmarkElement[i].setAttribute("src", "./assets/bookmark.png");
-      //Check if we are on the bookmark page
-      if (bookmarkBodyPage) {
-        bookmarkElement[i].parentNode.parentNode.classList.add("hidden");
-        bookmarkElement[i].parentNode.classList.add("hidden");
-      }
+const bookmarkElement = document.querySelectorAll(".question-card--bookmark");
+console.log(bookmarkElement.length);
+for (let i = 0; i < bookmarkElement.length; i++) {
+  console.log("FILL");
+  if (localStorage.getItem("bookMarkindex" + i) === "true") {
+    bookmarkElement[i].setAttribute("src", "./assets/bookmark_filled.png");
+    if (bookmarkBodyPage) {
+      bookmarkElement[i].parentNode.classList.add("hidden");
+    }
+  } else {
+    bookmarkElement[i].setAttribute("src", "./assets/bookmark.png");
+    //Check if we are on the bookmark page
+    if (bookmarkBodyPage) {
+      bookmarkElement[i].parentNode.parentNode.classList.add("hidden");
+      bookmarkElement[i].parentNode.classList.add("hidden");
     }
   }
 }
 
-//localStorage.setItem("bookMarkCounter", 0);
-
-//SET bookmark and bookmark counter
-if (bookmarkCounter) {
-  bookmarkCounter.textContent = localStorage.getItem("bookMarkCounter");
-}
 function ResetBookmark() {
   const bookmarkElement = document.querySelectorAll(".question-card--bookmark");
   for (let i = 0; i < bookmarkElement.length; i++) {
@@ -77,14 +136,15 @@ function ResetBookmark() {
         bookmarkElement[i].setAttribute("src", "./assets/bookmark_filled.png");
         let bookMarkCounter = localStorage.getItem("bookMarkCounter");
         bookMarkCounter = Number(bookMarkCounter) + 1;
-        localStorage.setItem("bookMarkindex" + i, true);
+        localStorage.setItem("bookMarkindex" + i, "true");
+        console.log(localStorage.getItem("bookMarkindex" + i));
         localStorage.setItem("bookMarkCounter", bookMarkCounter);
       } else {
         bookmarkElement[i].setAttribute("src", "./assets/bookmark.png");
         let bookMarkCounter = localStorage.getItem("bookMarkCounter");
         bookMarkCounter = Number(bookMarkCounter) - 1;
         localStorage.setItem("bookMarkCounter", bookMarkCounter);
-        localStorage.setItem("bookMarkindex" + i, false);
+        localStorage.setItem("bookMarkindex" + i, "false");
       }
     });
   }
@@ -134,16 +194,11 @@ if (form) {
 
     // ADD to the DOM
     const cardSection = document.createElement("section");
-    document.body.append(cardSection);
+
     cardSection.className = "question-card";
+    const main = document.querySelector('[data-js="form-main"]');
 
-    const bookmarkArea = document.createElement("a");
-    cardSection.append(bookmarkArea);
-
-    const bookmarkImage = document.createElement("img");
-    bookmarkArea.append(bookmarkImage);
-    bookmarkImage.className = "question-card--bookmark";
-    bookmarkImage.src = "./assets/bookmark.png";
+    main.append(cardSection);
 
     const questionParagraph = document.createElement("p");
     cardSection.append(questionParagraph);
@@ -174,20 +229,3 @@ if (form) {
     ResetBookmark();
   });
 }
-// Alternativ for loop
-/*
-answerButtonElement.forEach((button, i) => {
-  button.addEventListener("click", () => {
-    // const answerText = button.nextElementSibling;
-    answerTextElement[i].classList.toggle("hidden");
-
-    if (button.textContent === "Show Answer") {
-      button.textContent = "Hide Answer";
-      answerTextElement.textContent = "Flex-direction";
-    } else if ((button.textContent = "Hide Answer")) {
-      button.textContent = "Show Answer";
-      answerTextElement.textContent = "";
-    }
-  });
-});
-*/
